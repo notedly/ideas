dd23eb462c3ca8278998e4fc732276147dd1a345
megaton <lkh890111@gmail.com>
2018-12-05 18:56:02 +0900

    버튼 오버 작업중

--- html/js/index.js
+++ html/js/index.js
@@ -2,46 +2,123 @@ import React , { Component } from 'react' ;
 import ReactDOM , { render } from 'react-dom' ;
 // import { CustomSelectBox } from './ui/common' ;
 
+const Twave = (() => {
 
-class Twave{
-	constructor( tg ){
+	/*const comn = {
+		selectBoxAll : [] ,
+		activeClass : 'active' ,
+	} ;*/
 
-		this.items = [] ;
+	const TwaveWrap = ( tg, opts ) => {
 
-		if( tg ) {
-			this.items.push( tg ) ;
+		class Tw{
+
+			constructor( tg, opts ){
+				this.tag = {
+					tg : tg ,
+					tWrap : null ,
+					txt : null ,
+				} ;
+
+				this.opts = {
+					dir : opts.dir || 'up' ,
+				}
+				this.val = {
+					bot : 20 ,
+					btw : 2 ,
+				}
+
+				this.chkAni = false ;
+
+				this.render() ;
+				this.evtSet() ;
+
+				console.log( opts ) ;
+
+			}	// end of constructor
+
+			render(){
+
+				let txtWrap = document.createElement('p') ;
+				let txt = this.tag.tg.firstChild.textContent.split('');
+
+				this.tag.tg.innerHTML = '';
+				txt.forEach( (item, idx) => {
+					let elem = document.createElement('span') ;
+					elem.innerHTML = item;
+					txtWrap.append( elem );
+				}) ;
+
+				this.tag.tg.append( txtWrap );
+				this.tag.txt = this.tag.tg.querySelectorAll('span') ;
+				this.tag.tWrap = this.tag.tg.querySelector('p') ;
+
+				this.posReset() ;
+
+			}	// end of render
+
+			posReset( items ) {
+				let dir = 1 ;
+				// if( this.opts.dir == 'down' ) dir = -1 ;
+				if( this.tag.tg.classList.contains('wave_down') ) dir = -1 ;
+				[].forEach.call( this.tag.txt, (t ,idx) => {
+					t.style.transform = 'translate3d(0, ' + ( this.val.bot + idx * this.val.btw ) * dir + 'px, 0) rotateX(90deg)';
+					// t.style.animationDuration = '0.' + idx + 's';
+				}) ;
+			}
+
+			evtSet(){
+				let _this = this ;
+				this.tag.tg.addEventListener( 'mouseenter', function(){
+					_this.activeTxt() ;
+				}) ;
+
+				this.tag.tg.addEventListener( 'mouseleave', function(e){
+					_this.deActiveTxt() ;
+				}) ;
+			}
+
+			activeTxt(){
+				console.log( 'show!' ) ;
+				this.tag.tg.classList.add( 'active' ) ;
+			}	// end of activeTxt
+
+			deActiveTxt() {
+				console.log( 'hide!' ) ;
+				this.tag.tg.classList.remove( 'active' ) ;
+			}	// end of deActiveTxt
+
+
+
+		} // end of SelectBox
+
+		return new Tw( tg, opts ) ;
+	};
+
+	return ( option ) => {
+
+		let tg = document.querySelectorAll('.ani_twave')
+		,	 opts = option || {};
+
+		[].forEach.call( tg, item => TwaveWrap( item, opts ) ) ;
+
+		/*if( tg instanceof NodeList ) {
+			[].forEach.call( tg, btn => {
+				return TwaveWrap( btn ) ;
+			}) ;
 		} else {
-			this.items = document.querySelectorAll('.ani_twave') ;
-		}
-
-		this.init() ;
-
-	}	// end of constructor
-
-	splitText( tg ){
-		let txtWrap = document.createElement('p') ;
-		let txt = tg.firstChild.textContent.split('');
-
-		tg.innerHTML = '';
-		txt.forEach( (item, idx) => {
-			let elem = document.createElement('span') ;
-			// elem.style.transitionDelay = '0.'+idx +'s';
-			elem.innerHTML = item;
-			txtWrap.append( elem );
-		}) ;
-
-		tg.append( txtWrap );
-
-	}	// end of splitText
-
-	init(){
-		[].forEach.call( this.items, ( item ) => this.splitText( item )) ;
+			return TwaveWrap( tg ) ;
+		}*/
 	}
 
-}
+})() ;
+
+
 
 window.addEventListener('load', () => {
 
-	let a = new Twave() ;
+	Twave() ;
+	// Twave({ dir : 'down' }) ;
 
 }) ;
+
