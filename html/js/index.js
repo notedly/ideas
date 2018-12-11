ceede8881cf26c681e1ff5fc1dc320d3c8ee596a
megaton <lkh890111@gmail.com>
2018-12-11 18:59:47 +0900

    웨이브 작업중
    - 웨이브 svg 사용 작업중

--- html/js/index.js
+++ html/js/index.js
@@ -12,6 +12,14 @@ import ReactDOM , { render } from 'react-dom' ;
 	https://codepen.io/manpreet/pen/KwgBJN
 	https://www.tutorialspoint.com/svg/svg_rect.htm
 	https://codepen.io/jorgeatgu/pen/bEEemM
+
+
+	- 버튼 이벤트
+	 flag를 하나 넣어줘서 마우스이벤트가 있을경우, 없을경우 분기를 해준다!
+
+	- 웨이브 애니메이션
+	border-radius 사용
+
 */
 
 
@@ -32,25 +40,20 @@ const Twave = (() => {
 					tWrap : null ,
 					txt : null ,
 				} ;
-
-				this.opts = {
-					dir : opts.dir || 'up' ,
-				}
 				this.val = {
-					bot : 20 ,
+					bot : 10 ,
 					btw : 2 ,
 				}
 
 				this.chkAni = false ;
 
 				this.render() ;
-				this.evtSet() ;
 
 			}	// end of constructor
 
 			render(){
 
-				let txtWrap = document.createElement('p') ;
+				let txtWrap = document.createElement('div') ;
 				let txt = this.tag.tg.firstChild.textContent.split('');
 
 				this.tag.tg.innerHTML = '';
@@ -62,51 +65,20 @@ const Twave = (() => {
 
 				this.tag.tg.append( txtWrap );
 				this.tag.txt = this.tag.tg.querySelectorAll('span') ;
-				this.tag.tWrap = this.tag.tg.querySelector('p') ;
+				this.tag.tWrap = this.tag.tg.querySelector('div') ;
 
 				this.posReset() ;
 
 			}	// end of render
 
 			posReset( items ) {
-				let dir = 1 ;
-				// if( this.opts.dir == 'down' ) dir = -1 ;
-				if( this.tag.tg.classList.contains('wave_down') ) dir = -1 ;
 				[].forEach.call( this.tag.txt, (t ,idx) => {
-					t.style.transform = 'translate3d(0, ' + ( this.val.bot + idx * this.val.btw ) * dir + 'px, 0)';
+					let delay = ( ( idx + 1 ) * 0.02 ).toFixed(2) * 1;
+					console.log( 'delay : ', delay  ) ;
+					t.style.animationDelay = delay + 's';
+					// t.style.transform = 'translate3d(0, ' + ( this.val.bot + idx * this.val.btw ) * dir + 'px, 0)';
 				}) ;
 			}	// end of
-
-			evtSet(){
-
-				let _this = this ;
-
-				this.tag.tg.addEventListener( 'mouseenter', function(){
-					_this.activeTxt() ;
-				}) ;
-
-				this.tag.tg.addEventListener( 'mouseleave', function(e){
-					_this.deActiveTxt() ;
-				}) ;
-
-				/*this.tag.tg.addEventListener( 'webkitTransitionEnd', function(evt) {
-					console.log( evt.propertyName ) ;
-				})*/
-
-
-			}
-
-			activeTxt(){
-				console.log( 'show!' ) ;
-				this.tag.tg.classList.add( 'active' ) ;
-			}	// end of activeTxt
-
-			deActiveTxt() {
-				console.log( 'hide!' ) ;
-				this.tag.tg.classList.remove( 'active' ) ;
-			}	// end of deActiveTxt
-
-
 
 		} // end of SelectBox
 
@@ -115,7 +87,7 @@ const Twave = (() => {
 
 	return ( option ) => {
 
-		let tg = document.querySelectorAll('.ani_twave')
+		let tg = document.querySelectorAll('.ani_twave_1')
 		,	 opts = option || {};
 
 		[].forEach.call( tg, item => TwaveWrap( item, opts ) ) ;
@@ -131,12 +103,149 @@ const Twave = (() => {
 
 })() ;
 
+const Twave2 = (() => {
+
+	const TwaveWrap = ( tg, opts ) => {
+
+		class Tw{
+
+			constructor( tg, opts ){
+				this.tag = {
+					tg : tg ,
+					tWrap : null ,
+					txt : null ,
+				} ;
+				this.val = {
+					bot : 10 ,
+					btw : 2 ,
+				}
+				this.chkAni = false ;
+				this.render() ;
+
+			}	// end of constructor
+
+			render(){
+
+				let txtWrap = document.createElement('div') ;
+				let txt = this.tag.tg.firstChild.textContent.split('');
+
+				this.tag.tg.innerHTML = '';
+				txt.forEach( (item, idx) => {
+					let elem = document.createElement('span') ;
+					elem.innerHTML = item;
+					txtWrap.append( elem );
+				}) ;
+
+				this.tag.tg.append( txtWrap );
+				this.tag.txt = this.tag.tg.querySelectorAll('span') ;
+				this.tag.tWrap = this.tag.tg.querySelector('div') ;
+
+				this.posReset() ;
+
+			}	// end of render
+
+			posReset( items ) {
+				[].forEach.call( this.tag.txt, (t ,idx) => {
+					let tDelay = ( ( idx + 1 ) * 0.05 ).toFixed(2) * 1;
+					let aDelay = ( tDelay + ( this.tag.txt.length/2 * 0.1) ).toFixed(2) * 1;
+					console.log( 'delay : ', tDelay + '/' + aDelay ) ;
+					t.style.transitionDelay = tDelay + 's';
+					t.style.animationDelay = aDelay + 's';
+					// t.style.transform = 'translate3d(0, ' + ( this.val.bot + idx * this.val.btw ) * dir + 'px, 0)';
+				}) ;
+			}	// end of
+
+		} // end of Tw
+
+		return new Tw( tg, opts ) ;
+	};
+
+	return ( option ) => {
+
+		let tg = document.querySelectorAll('.ani_twave_2')
+		,	 opts = option || {};
+
+		[].forEach.call( tg, item => TwaveWrap( item, opts ) ) ;
+
+	}
+
+})() ;
+
+const Bwave = (() => {
+
+	const TwaveWrap = ( tg, opts ) => {
+
+		class Tw{
+
+			constructor( tg, opts ){
+				this.tag = {
+					tg : tg ,
+					tWrap : null ,
+					txt : null ,
+				} ;
+				this.val = {
+					bot : 10 ,
+					btw : 2 ,
+				}
+				this.chkAni = false ;
+				this.render() ;
+
+			}	// end of constructor
+
+			render(){
+
+				let txtWrap = document.createElement('div') ;
+				let txt = this.tag.tg.firstChild.textContent.split('');
+
+				this.tag.tg.innerHTML = '';
+				txt.forEach( (item, idx) => {
+					let elem = document.createElement('span') ;
+					elem.innerHTML = item;
+					txtWrap.append( elem );
+				}) ;
+
+				this.tag.tg.append( txtWrap );
+				this.tag.txt = this.tag.tg.querySelectorAll('span') ;
+				this.tag.tWrap = this.tag.tg.querySelector('div') ;
+
+				this.posReset() ;
+
+			}	// end of render
+
+			posReset( items ) {
+				[].forEach.call( this.tag.txt, (t ,idx) => {
+					let tDelay = ( ( idx + 1 ) * 0.05 ).toFixed(2) * 1;
+					let aDelay = ( tDelay + ( this.tag.txt.length/2 * 0.1) ).toFixed(2) * 1;
+					console.log( 'delay : ', tDelay + '/' + aDelay ) ;
+					t.style.transitionDelay = tDelay + 's';
+					t.style.animationDelay = aDelay + 's';
+					// t.style.transform = 'translate3d(0, ' + ( this.val.bot + idx * this.val.btw ) * dir + 'px, 0)';
+				}) ;
+			}	// end of
+
+		} // end of Tw
+
+		return new Tw( tg, opts ) ;
+	};
+
+	return ( option ) => {
+
+		let tg = document.querySelectorAll('.ani_bwave')
+		,	 opts = option || {};
+
+		[].forEach.call( tg, item => TwaveWrap( item, opts ) ) ;
+
+	}
+
+})() ;
 
 
 window.addEventListener('load', () => {
 
 	Twave() ;
-	// Twave({ dir : 'down' }) ;
+	Twave2() ;
+	Bwave() ;
+
 
 	let btn = document.querySelector('.btn_link') ;
 	btn.addEventListener('mouseenter', function(){
@@ -144,5 +253,239 @@ window.addEventListener('load', () => {
 		document.querySelector('#aniSvg').beginElement();
 	})
 
+
+
 }) ;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class ShapeOverlays {
+  constructor(elm) {
+    this.elm = elm;
+    this.path = elm.querySelectorAll('path');
+    this.numPoints = 4;
+    this.duration = 1000;
+    this.delayPointsArray = [];
+    this.delayPointsMax = 0;
+    this.delayPerPath = 60;
+    this.timeStart = Date.now();
+    this.isOpened = false;
+    this.isAnimating = false;
+  }
+  toggle() {
+    this.isAnimating = true;
+    for (var i = 0; i < this.numPoints; i++) {
+      this.delayPointsArray[i] = 0;
+    }
+    if (this.isOpened === false) {
+      this.open();
+    } else {
+      this.close();
+    }
+  }
+  open() {
+    this.isOpened = true;
+    this.elm.classList.add('is-opened');
+    this.timeStart = Date.now();
+    this.renderLoop();
+  }
+  close() {
+    this.isOpened = false;
+    this.elm.classList.remove('is-opened');
+    this.timeStart = Date.now();
+    this.renderLoop();
+  }
+  updatePath(time) {
+    const points = [];
+    for (var i = 0; i < this.numPoints; i++) {
+      const thisEase = (i % 2 === 1) ? ease.sineOut : ease.exponentialInOut;
+      points[i] = (1 - thisEase(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1))) * 100
+    }
+
+    let str = '';
+    str += (this.isOpened) ? `M 0 0 H ${points[0]}` : `M ${points[0]} 0`;
+    for (var i = 0; i < this.numPoints - 1; i++) {
+      const p = (i + 1) / (this.numPoints - 1) * 100;
+      const cp = p - (1 / (this.numPoints - 1) * 100) / 2;
+      str += `C ${points[i]} ${cp} ${points[i + 1]} ${cp} ${points[i + 1]} ${p} `;
+    }
+    str += (this.isOpened) ? `H 100 V 0` : `H 0 V 0`;
+    return str;
+  }
+  render() {
+    if (this.isOpened) {
+      for (var i = 0; i < this.path.length; i++) {
+        this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * i)));
+      }
+    } else {
+      for (var i = 0; i < this.path.length; i++) {
+        this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
+      }
+    }
+  }
+  renderLoop() {
+    this.render();
+    if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPointsMax) {
+      requestAnimationFrame(() => {
+        this.renderLoop();
+      });
+    }
+    else {
+      this.isAnimating = false;
+    }
+  }
+}
+
+(function() {
+  const elmHamburger = document.querySelector('.hamburger');
+  const gNavItems = document.querySelectorAll('.global-menu__item');
+  const elmOverlay = document.querySelector('.shape-overlays');
+  const overlay = new ShapeOverlays(elmOverlay);
+
+  elmHamburger.addEventListener('click', () => {
+    if (overlay.isAnimating) {
+      return false;
+    }
+    overlay.toggle();
+    if (overlay.isOpened === true) {
+      elmHamburger.classList.add('is-opened-navi');
+      for (var i = 0; i < gNavItems.length; i++) {
+        gNavItems[i].classList.add('is-opened');
+      }
+    } else {
+      elmHamburger.classList.remove('is-opened-navi');
+      for (var i = 0; i < gNavItems.length; i++) {
+        gNavItems[i].classList.remove('is-opened');
+      }
+    }
+  });
+}());
+
+
+
+
+class ShapeOverlays {
+  constructor(elm) {
+    this.elm = elm;
+    this.path = elm.querySelectorAll('path');
+    this.numPoints = 2;
+    this.duration = 600;
+    this.delayPointsArray = [];
+    this.delayPointsMax = 0;
+    this.delayPerPath = 200;
+    this.timeStart = Date.now();
+    this.isOpened = false;
+    this.isAnimating = false;
+  }
+  toggle() {
+    this.isAnimating = true;
+    for (var i = 0; i < this.numPoints; i++) {
+      this.delayPointsArray[i] = 0;
+    }
+    if (this.isOpened === false) {
+      this.open();
+    } else {
+      this.close();
+    }
+  }
+  open() {
+    this.isOpened = true;
+    this.elm.classList.add('is-opened');
+    this.timeStart = Date.now();
+    this.renderLoop();
+  }
+  close() {
+    this.isOpened = false;
+    this.elm.classList.remove('is-opened');
+    this.timeStart = Date.now();
+    this.renderLoop();
+  }
+  updatePath(time) {
+    const points = [];
+    for (var i = 0; i < this.numPoints; i++) {
+      const thisEase = this.isOpened ?
+                        (i == 1) ? ease.cubicOut : ease.cubicInOut:
+                        (i == 1) ? ease.cubicInOut : ease.cubicOut;
+      points[i] = thisEase(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1)) * 100
+    }
+
+    let str = '';
+    str += (this.isOpened) ? `M 0 0 V ${points[0]} ` : `M 0 ${points[0]} `;
+    for (var i = 0; i < this.numPoints - 1; i++) {
+      const p = (i + 1) / (this.numPoints - 1) * 100;
+      const cp = p - (1 / (this.numPoints - 1) * 100) / 2;
+      str += `C ${cp} ${points[i]} ${cp} ${points[i + 1]} ${p} ${points[i + 1]} `;
+    }
+    str += (this.isOpened) ? `V 0 H 0` : `V 100 H 0`;
+    return str;
+  }
+  render() {
+    if (this.isOpened) {
+      for (var i = 0; i < this.path.length; i++) {
+        this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * i)));
+      }
+    } else {
+      for (var i = 0; i < this.path.length; i++) {
+        this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
+      }
+    }
+  }
+  renderLoop() {
+    this.render();
+    if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPointsMax) {
+      requestAnimationFrame(() => {
+        this.renderLoop();
+      });
+    }
+    else {
+      this.isAnimating = false;
+    }
+  }
+}
+*/
+/*
+(function() {
+  const elmHamburger = document.querySelector('.hamburger');
+  const gNavItems = document.querySelectorAll('.global-menu__item');
+  const elmOverlay = document.querySelector('.shape-overlays');
+  const overlay = new ShapeOverlays(elmOverlay);
+
+  elmHamburger.addEventListener('click', () => {
+    if (overlay.isAnimating) {
+      return false;
+    }
+    overlay.toggle();
+    if (overlay.isOpened === true) {
+      elmHamburger.classList.add('is-opened-navi');
+      for (var i = 0; i < gNavItems.length; i++) {
+        gNavItems[i].classList.add('is-opened');
+      }
+    } else {
+      elmHamburger.classList.remove('is-opened-navi');
+      for (var i = 0; i < gNavItems.length; i++) {
+        gNavItems[i].classList.remove('is-opened');
+      }
+    }
+  });
+}());*/
--- server/app.js
+++ server/app.js
@@ -1,28 +1,32 @@
-import express from 'express' ; 
-import session from 'express-session' ; 
-import mysql from 'mysql' ; 
-import ejs from 'ejs' ; 
-import fs from 'fs' ; 
-import { PATH } from '../dir' ; 
+import express from 'express' ;
+import session from 'express-session' ;
+import mysql from 'mysql' ;
+import ejs from 'ejs' ;
+import fs from 'fs' ;
+import { PATH } from '../dir' ;
 
-const [ app , DIR ] = [ express() , PATH.DIR ] ; 
+const [ app , DIR ] = [ express() , PATH.DIR ] ;
 
-app.set( 'views' , __dirname + '/../html_build/' ) ; 
-app.set( 'view engine' , 'ejs' ) ; 
-app.engine( 'html' , require( 'ejs' ).renderFile ) ;  
-app.use( '/' , express.static( __dirname + '/../html_build' )) ; 
+app.set( 'views' , __dirname + '/../html_build/' ) ;
+app.set( 'view engine' , 'ejs' ) ;
+app.engine( 'html' , require( 'ejs' ).renderFile ) ;
+app.use( '/' , express.static( __dirname + '/../html_build' )) ;
 
 /**
-@기본 : 
+@기본 :
 app.get( '/' , ( req , res ) => {
-	res.render( 'index' , {}) ; 
-}) ; 
+	res.render( 'index' , {}) ;
+}) ;
 */
 
 /* Routing List ( get ) */
-app.get( '/' , ( req , res ) => res.render( 'index' , {}) ) ; 
+app.get( '/' , ( req , res ) => res.render( 'index' , {}) ) ;
+
+app.get( '/text_wave_01' , ( req , res ) => res.render( 'text_wave_01' , {}) ) ;
+app.get( '/text_wave_02' , ( req , res ) => res.render( 'text_wave_02' , {}) ) ;
+app.get( '/box_wave' , ( req , res ) => res.render( 'box_wave' , {}) ) ;
 
 /* Server */
 const server = app.listen( DIR.PORT , () => {
 	console.log( 'Express listening on port : ' +  server.address().port) ;
-}) ; 
+}) ;
