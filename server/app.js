64c9680acd9cbe12af245ade2ef05d4544657aa5
megaton <lkh890111@gmail.com>
2019-01-03 11:35:26 +0900

    작업 폴더 구조 변경

--- server/app.js
+++ server/app.js
@@ -1,28 +1,29 @@
-import express from 'express' ; 
-import session from 'express-session' ; 
-import mysql from 'mysql' ; 
-import ejs from 'ejs' ; 
-import fs from 'fs' ; 
-import { PATH } from '../dir' ; 
+import express from 'express' ;
+import session from 'express-session' ;
+import mysql from 'mysql' ;
+import ejs from 'ejs' ;
+import fs from 'fs' ;
+import { PATH } from '../dir' ;
 
-const [ app , DIR ] = [ express() , PATH.DIR ] ; 
+const [ app , DIR ] = [ express() , PATH.DIR ] ;
 
-app.set( 'views' , __dirname + '/../html_build/' ) ; 
-app.set( 'view engine' , 'ejs' ) ; 
-app.engine( 'html' , require( 'ejs' ).renderFile ) ;  
-app.use( '/' , express.static( __dirname + '/../html_build' )) ; 
+app.set( 'views' , __dirname + '/../html_build/' ) ;
+app.set( 'view engine' , 'ejs' ) ;
+app.engine( 'html' , require( 'ejs' ).renderFile ) ;
+app.use( '/' , express.static( __dirname + '/../html_build' )) ;
 
 /**
-@기본 : 
+@기본 :
 app.get( '/' , ( req , res ) => {
-	res.render( 'index' , {}) ; 
-}) ; 
+	res.render( 'index' , {}) ;
+}) ;
 */
 
 /* Routing List ( get ) */
-app.get( '/' , ( req , res ) => res.render( 'index' , {}) ) ; 
+app.get( '/' , ( req , res ) => res.render( 'index' , {}) ) ;
+app.get( '/css_slider_radio' , ( req , res ) => res.render( 'css_slider_radio' , {}) ) ;
 
 /* Server */
 const server = app.listen( DIR.PORT , () => {
 	console.log( 'Express listening on port : ' +  server.address().port) ;
-}) ; 
+}) ;
