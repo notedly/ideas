7f037855b8668bcdfb74fe9176b3ffdd696e8969
megaton <lkh890111@gmail.com>
2019-01-24 16:39:00 +0900

    20190124 스터디 복습

--- server/app.js
+++ server/app.js
@@ -24,6 +24,8 @@ app.get( '/' , ( req , res ) => res.render( 'index' , {}) ) ;
 app.get( '/css_slider_radio' , ( req , res ) => res.render( 'css_slider_radio' , {}) ) ;
 app.get( '/image_split_slide' , ( req , res ) => res.render( 'image_split_slide' , {}) ) ;
 app.get( '/css_grid_layout' , ( req , res ) => res.render( 'css_grid_layout' , {}) ) ;
+app.get( '/css_grid_layout2' , ( req , res ) => res.render( 'css_grid_layout2' , {}) ) ;
+app.get( '/css_grid_layout3' , ( req , res ) => res.render( 'css_grid_layout3' , {}) ) ;
 
 /* Server */
 const server = app.listen( DIR.PORT , () => {
