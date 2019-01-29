96780334e3728919dd575d76cfc4fc1bf599e02a
megaton <lkh890111@gmail.com>
2019-01-29 12:19:07 +0900

    2019.01.29 스터디 복습

--- server/app.js
+++ server/app.js
@@ -27,6 +27,7 @@ app.get( '/css_grid_layout' , ( req , res ) => res.render( 'css_grid_layout' , {
 app.get( '/css_grid_layout2' , ( req , res ) => res.render( 'css_grid_layout2' , {}) ) ;
 app.get( '/css_grid_layout4' , ( req , res ) => res.render( 'css_grid_layout4' , {}) ) ;
 app.get( '/css_grid_layout5' , ( req , res ) => res.render( 'css_grid_layout5' , {}) ) ;
+app.get( '/css_grid_layout6' , ( req , res ) => res.render( 'css_grid_layout6' , {}) ) ;
 
 /* Server */
 const server = app.listen( DIR.PORT , () => {
